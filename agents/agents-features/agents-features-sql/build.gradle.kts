c34e41e254ab15b0395ccfb58458d45f98906673
Mark.Tkachenko <mark.tkachenko@jetbrains.com>
2025-09-01 13:01:00 +0200

    fix project referece

--- agents/agents-features/agents-features-sql/build.gradle.kts
+++ agents/agents-features/agents-features-sql/build.gradle.kts
@@ -13,7 +13,6 @@ kotlin {
         commonMain {
             dependencies {
                 api(project(":agents:agents-core"))
-                api(project(":agents:agents-features:agents-features-common"))
                 api(project(":agents:agents-features:agents-features-snapshot"))
                 api(project(":rag:rag-base"))
 
